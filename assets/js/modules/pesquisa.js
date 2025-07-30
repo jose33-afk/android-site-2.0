f8589898d2072915b724cec72f124c7305f674c9
Jose <josedev337@gmail.com>
2025-07-30 19:57:41 -0300

    fase inicial sistema de pesquisa
    funcionamento basico funcionando, nao completado.

--- assets/js/modules/pesquisa.js
+++ assets/js/modules/pesquisa.js
@@ -1,5 +1,46 @@
 import { pegaElemento } from "./utils.js";
 
+const nomesPesquisar = [ 
+    {"AndyRubin":"andy-rubin-pai-do-android"},
+    {"RichMiner":"rich-miner-cofundador"}
+]
+
+/*
+let inputPesquisa = "RichMiner";//teste, deixar tudo mais usculas e sem espaços
+
+let nomeDoIdDoElemento;
+
+nomesPesquisar.forEach(nome => {
+    const nomeDoOjeto = Object.keys(nome);
+    
+    if (nomeDoOjeto == inputPesquisa) {
+        nomeDoIdDoElemento = nome[nomeDoOjeto] // 1
+    }
+    rolar(nomeDoIdDoElemento)
+})
+*/
+
+
+function rolar(IdElement) { //FOIIIIIIIIIIIIIIIIII
+   // const element = document.querySelector(`#${IdElement}`)
+   const element = document.querySelector('#recursos-complementares-e-interatividade')
+    const posicaoDoElement = element.offsetTop;
+    console.log(posicaoDoElement)
+
+   setTimeout(() => {
+        window.scrollTo({
+            top:posicaoDoElement,
+            behavior: 'smooth'
+        })
+    }, 1000)
+}
+
+
+rolar()
+
+
+
+
 function pesquisa () {
     const input = pegaElemento('#pesquisa__input');
 
@@ -16,3 +57,7 @@ function pesquisa () {
 }
 
 export { pesquisa };
+
+/*
+    1 - é porque eu so tenho um elemento dentro e ele é o AndyRubin ou outro.
+*/
